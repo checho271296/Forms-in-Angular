d75beb02d79e7c1ab2e24a2e8667ff35046470a2
checho271296 <sfonseca@estudiantec.cr>
2020-12-07 00:13:02 -0600

    add valitation and new group form

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -14,9 +14,48 @@ export class ReactiveComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {
     this.createForm();
+    this.loadDataToForm();
    }
 
   ngOnInit(): void {
+  }
+
+  get noValidName():Boolean{
+    return this.forma.get('name').invalid &&  this.forma.get('name').touched;
+  }
+
+  get noValidLastname():Boolean{
+    return this.forma.get('lastname').invalid &&  this.forma.get('lastname').touched;
+  }
+
+  get noValidMail():Boolean{
+    return this.forma.get('mail').invalid &&  this.forma.get('mail').touched;
+  }
+
+  get noValidState():Boolean{
+    return this.forma.get('address.state').invalid &&  this.forma.get('address.state').touched;
+  }
+
+  get noValidCity():Boolean{
+    return this.forma.get('address.city').invalid &&  this.forma.get('address.city').touched;
+  }
+
+  get validName():Boolean{
+    return this.forma.get('name').valid; 
+  }
+
+  get validLastname():Boolean{
+    return this.forma.get('lastname').valid;
+  }
+
+  get validMail():Boolean{
+    return this.forma.get('mail').valid ;
+  }
+  get validState():Boolean{
+    return this.forma.get('address.state').valid ;
+  }
+  get validCity():Boolean{
+    return this.forma.get('address.city').valid ;
   }
 
   createForm(){
@@ -24,12 +63,41 @@ export class ReactiveComponent implements OnInit {
     this.forma = this.formBuilder.group({
       name  : ['',[Validators.required,Validators.minLength(5)]],
       lastname: ['',[Validators.required,Validators.minLength(5)]],
-      mail  : ['',[Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]]
+      mail  : ['',[Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
+      address: this.formBuilder.group({
+        state: ['',Validators.required],
+        city: ['',Validators.required]
+      })
+    });
+  }
+
+  loadDataToForm(){
+    this.forma.setValue({
+      name: "Sergio",
+      lastname: "Fonseca",
+      mail: "sergio@gmail.com",
+      address: {
+        state: "San Juan",
+        city: "Cartago"
+      }
     });
   }
 
   save(){
-      console.log(this.forma);
+    if(this.forma.valid){
+      console.log(this.forma.value);
+    }else{
+      Object.values(this.forma.controls).forEach(control =>{
+        if (control instanceof FormGroup){
+          Object.values(control.controls).forEach(subControl =>{
+            subControl.markAsTouched();
+          });
+          };
+        control.markAsTouched();
+      });
+    };
+
+    this.forma.reset();
   }
 
 }
